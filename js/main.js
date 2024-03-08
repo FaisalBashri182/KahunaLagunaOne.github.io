570527bed3d69919fb3fe1333b3e2b95b81d338d
Faisal <faisalbashri182@gmail.com>
2024-03-08 14:13:11 +0700

    Perbaikan dan penambahan

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
  const mntoggle = document.querySelector('.menu-toggle input');
  const nav = document.querySelector('nav ul');
+ function myFunction(imgs) {
+    // Get the expanded image
+    var expandImg = document.getElementById("expandedImg");
+    // Get the image text
+    var imgText = document.getElementById("imgtext");
+    // Use the same src in the expanded image as the image being clicked on from the grid
+    expandImg.src = imgs.src;
+    // Use the value of the alt attribute of the clickable image as text inside the expanded image
+    imgText.innerHTML = imgs.alt;
+    // Show the container element (hidden with CSS)
+    expandImg.parentElement.style.display = "block";
+  }
 
 mntoggle.addEventListener('click',function(){
     nav.classList.toggle('menushow');
